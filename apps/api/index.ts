b268f04a417e51f6a787441f71ac8936d2e43d41
ashish <ashishkushwaha472004@gmail.com>
2026-01-24 00:32:48 +0530

    add signup route

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import { prismaclient } from 'store/client';
+import { AuthInput } from './types';
+import { password } from 'bun';
 
 const app = express();
 
@@ -20,7 +22,31 @@ app.get("/website", async (req, res) => {
 })
 
 app.post("/status/:websiteId", (req, res) => {
-  console.log('something');
+})
+
+app.post("/user/signin", (req, res) => {
+  const data = AuthInput.safeParse(req.body.data);
+  if (!data.success) {
+    res.status(403).send("")
+  }
+})
+
+app.post("/user/signup", async (req, res) => {
+  const data = AuthInput.safeParse(req.body.data);
+  if (!data.success) {
+    res.status(403).send("")
+  }
+
+  try {
+    await prismaclient.user.create({
+    data: {
+      username: data.data?.username,
+      password: data.data?.password,
+    }
+  })
+  } catch (error) {
+    res.status(403).send(""); // 31:50
+  }
 })
 
 app.listen("3001", (err) => {
---
+++ apps/api/types.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const AuthInput = z.object({
+  username: z.string(),
+  password: z.string()
+});
+
